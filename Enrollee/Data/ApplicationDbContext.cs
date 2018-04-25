2fc6a0eb296adc9040b11f682ab243250fd33ad2
TheAshen1 <theexaltedmax@gmail.com>
2018-04-25 19:46:41 +0300

    Add project files.

---
+++ Enrollee/Data/ApplicationDbContext.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Enrollee.Models;
+
+namespace Enrollee.Data
+{
+    public class ErolleeDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ErolleeDbContext(DbContextOptions<ErolleeDbContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+            // Customize the ASP.NET Identity model and override the defaults if needed.
+            // For example, you can rename the ASP.NET Identity table names and more.
+            // Add your customizations after calling base.OnModelCreating(builder);
+        }
+    }
+}
